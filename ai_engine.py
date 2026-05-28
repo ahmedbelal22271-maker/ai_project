4e46062633506617f272f26c968fc10ebfe21035
saifallah <saifallahbasem2@gmail.com>
2026-05-28 10:38:50 +0300

    Create AIPlayer class scaffold and basic move dispatcher

--- ai_engine.py
+++ ai_engine.py
@@ -1,1 +1,20 @@
-class AIPlayer: pass
+import random
+from constants import GRID_SIZE
+
+class AIPlayer:
+    def __init__(self, difficulty, engine):
+        self.difficulty = difficulty
+        self.engine = engine
+
+    def get_move(self):
+        if self.difficulty == 1:
+            return self._ai_easy()
+            
+        valid_moves = self.engine.get_valid_pawn_moves(2)
+        if valid_moves:
+            return ('MOVE', random.choice(valid_moves))
+        return None
+
+    def _ai_easy(self):
+        valid_moves = self.engine.get_valid_pawn_moves(2)
+        return ('MOVE', random.choice(valid_moves))
